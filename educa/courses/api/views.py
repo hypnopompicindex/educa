fb2ae1379e9bce1c1bf30f0000028a1b37cd8216
hypnopompicindex <hypnopompicindex@gmail.com>
2016-10-21 18:17:14 -0400

    added permissions to views

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,5 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics
-from ..models import Subject
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from ..models import Subject, Course
 from .serializers import SubjectSerializer
 
 
@@ -11,3 +16,14 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+
+
+class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
+
