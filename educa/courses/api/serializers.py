76e2324429ddde587bcfaae2fd8bc1d696260227
hypnopompicindex <hypnopompicindex@gmail.com>
2016-10-24 16:57:05 -0400

    added course contents serialization  to api

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,12 +1,25 @@
 from rest_framework import serializers
 from ..models import Subject
-from ..models import Course, Module
+from ..models import Course, Module, Content
 
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ('id', 'title', 'slug')
+
+
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+
+    class Meta:
+        model = Content
+        fields = ('order', 'item')
 
 
 class ModuleSerializer(serializers.ModelSerializer):
@@ -21,3 +34,20 @@ class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
         fields = ('id', 'subject', 'title', 'slug', 'overview', 'created', 'owner', 'modules')
+
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+
+    class Meta:
+        model = Module
+        fields = ('order', 'title', 'description', 'contents')
+
+
+class CourseWithContentsSerializer(CourseSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+
+    class Meta:
+        model = Course
+        fields = ('id', 'subject', 'title', 'slug',
+                  'overview', 'created', 'owner', 'modules')
